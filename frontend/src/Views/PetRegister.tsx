05d34dfde518fc3298af795fa4499afba5c23658
Fernanda Montalvan <fernanda.r.montalvan@gmail.com>
2025-08-19 20:25:48 +0200

    feat: Adding missing text on HomePage - Fixing translation context for PetRegister.

--- frontend/src/Views/PetRegister.tsx
+++ frontend/src/Views/PetRegister.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import ThemeProvider from '../Components/theme/ThemeProvider';
-import LanguageProvider from '../i18n/LanguageProvider';
 import { useTranslation } from '../i18n/hooks/hook';
 import NavBar from '../Components/NavBar';
 import Footer from '../Components/footer';
@@ -8,18 +6,16 @@ import Footer from '../Components/footer';
 const PetRegister: React.FC = () => {
   const { translate } = useTranslation();
   return (
-    <LanguageProvider>
-      <ThemeProvider>
-        <NavBar />
-          <div className="flex flex-col items-center justify-center min-h-screen bg-[#FDF2DE] dark:bg-[#51344D]">
-            <h1 className="h1 font-caprasimo mb-4">{translate('welcome')}</h1>
-            <p className="lead text-[--huellas-eggplant]/80 dark:text-[--huellas-ice]">
-              Registro de Mascotas
-            </p>
-          </div>
-        <Footer />
-      </ThemeProvider>
-    </LanguageProvider>
+    <>
+      <NavBar />
+        <div className="flex flex-col items-center justify-center min-h-screen bg-[#FDF2DE] dark:bg-[#51344D]">
+          <h1 className="h1 font-caprasimo mb-4">{translate('welcome')}</h1>
+          <p className="lead text-[--huellas-eggplant]/80 dark:text-[--huellas-ice]">
+            Registro de Mascotas
+          </p>
+        </div>
+      <Footer />
+    </>
   );
 };
 
