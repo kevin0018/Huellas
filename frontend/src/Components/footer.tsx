a484f123d126ccafd1c9c04965aa9bc5e798fc57
Fernanda Montalvan <fernanda.r.montalvan@gmail.com>
2025-08-19 18:03:05 +0200

    feat: Update Footer icons size and adjust HomePage layout for better responsiveness

--- frontend/src/Components/footer.tsx
+++ frontend/src/Components/footer.tsx
@@ -9,9 +9,9 @@ const Footer: React.FC = () => {
     <footer className="bg-[#A89B9D] text-white p-4 shadow-md w-full sticky top-0 z-50 rounded-xs">
       <div className="container mx-auto py-4 flex flex-col justify-center items-center md:flex-row md:justify-between">
         <div className="flex justify-center space-x-4 py-1.5">
-          <img src="/media/sm/facebook.png" alt="facebook" className="w-6 h-6" />
-          <img src="/media/sm/instagram.png" alt="instagram" className="w-6 h-6" />
-          <img src="/media/sm/linkedin.png" alt="linkedin" className="w-6 h-6" />
+          <img src="/media/sm/facebook.png" alt="facebook" className="w-7 h-7" />
+          <img src="/media/sm/instagram.png" alt="instagram" className="w-7 h-7" />
+          <img src="/media/sm/linkedin.png" alt="linkedin" className="w-7 h-7" />
         </div>
         <div className="flex flex-row items-center space-x-4 py-1.5">
           <Link to="/login" className="text-sm hover-eggplant">{translate('login')}</Link>
