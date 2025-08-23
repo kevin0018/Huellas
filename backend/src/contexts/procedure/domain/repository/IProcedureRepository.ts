9711f193c8c377f6f2146d811cabd8b837d31853
adriElias <34091624+adriElias@users.noreply.github.com>
2025-08-23 18:25:27 +0200

    feat: Add get procedure by animal type endpoint

---
+++ backend/src/contexts/procedure/domain/repository/IProcedureRepository.ts
@@ -0,0 +1,6 @@
+import { PetType } from "@prisma/client";
+import { Procedure } from "../entities/Procedure.js";
+
+export interface IProcedureRepository{
+    findByPetType(type: PetType): Promise<Procedure[]>
+}
